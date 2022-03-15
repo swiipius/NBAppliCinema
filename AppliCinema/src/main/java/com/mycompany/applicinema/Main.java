9798bdd4031319f247388e5cf17f6b8cfa5b25c5
Pierre <70819571+swiipius@users.noreply.github.com>
2022-03-15 08:58:30 +0100

    Test1
    Premier push

---
+++ AppliCinema/src/main/java/com/mycompany/applicinema/Main.java
@@ -0,0 +1,18 @@
+package com.mycompany.applicinema;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+import java.util.*;
+
+/**
+ *
+ * @author pierr
+ */
+public class Main {
+    public static void test(String[] args){
+        System.out.println("test");
+    }
+}
