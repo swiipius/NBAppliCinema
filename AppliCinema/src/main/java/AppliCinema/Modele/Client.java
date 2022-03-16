dcc240f930c35253e85875fcbc62ffe560f9f91d
HugoDerMatheossian <78907879+HugoDerMatheossian@users.noreply.github.com>
2022-03-16 13:58:05 +0100

    HugoCommit1
    créations de nouvelles classes+attributs

---
+++ AppliCinema/src/main/java/AppliCinema/Modele/Client.java
@@ -0,0 +1,14 @@
+package AppliCinema.Modele;
+
+/**
+ *
+ * @author prohd
+ */
+public class Client {
+    String login,nom,prenom,status;
+    int age;
+    float reduction;
+    public Client(){
+        
+    }
+}
---
+++ AppliCinema/src/main/java/AppliCinema/Modele/ClientInvite.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package AppliCinema.Modele;
+
+/**
+ *
+ * @author prohd
+ */
+public class ClientInvite extends Client {
+    public ClientInvite(){
+        reduction=0;
+        status="invite";
+    }
+}
