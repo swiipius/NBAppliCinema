836991e1dac02a84638d49e794c4f8c781e7fcd8
HugoDerMatheossian <78907879+HugoDerMatheossian@users.noreply.github.com>
2022-03-22 11:06:00 +0100

    maj 8.2
    version 8.2

--- AppliCinema/src/jdbc2020/Controleur.java
+++ AppliCinema/src/jdbc2020/Controleur.java
@@ -26,17 +26,18 @@ public class Controleur {
     @SuppressWarnings("empty-statement")
     public static void main(String[] s) throws SQLException, ClassNotFoundException {
         // creation de la fenetre
-        //Fenetre f = new Fenetre();
-        Connexion connect = new Connexion("Cinema", "root", "");//connection a la bdd reussie
+        Fenetre f = new Fenetre();
+        f.setVisible(true);
+        //Connexion connect = new Connexion("Cinema", "root", "");//connection a la bdd reussie
         /*String requeteAjout = "INSERT INTO film (Titre,NomRealisateur,PrenomRealisateur,duree,genre,note,synopsis) VALUES ('The King s Man:Premiere Mission','Vaugh',' Matthew','131','espionnage',3.4,'Lorsque les pires tyrans et les plus grands génies criminels de l’Histoire se réunissent pour planifier l’élimination de millions d’innocents, un homme se lance dans une course contre la montre pour contrecarrer leurs plans. ')";
         connect.executeUpdate(requeteAjout);*/
         /*String requeteSupprimer = "DELETE FROM Film WHERE Titre LIKE 'The Batman' ";
         connect.executeUpdate(requeteSupprimer);*/
-        String requeteAjout = "INSERT INTO film (Titre,NomRealisateur,PrenomRealisateur,duree,genre,note,synopsis) VALUES ('Test','Test',' test','0','test',3.4,'Lorsque les pires tyrans et les plus grands génies criminels de l’Histoire se réunissent pour planifier l’élimination de millions d’innocents, un homme se lance dans une course contre la montre pour contrecarrer leurs plans. ')";
+        /*String requeteAjout = "INSERT INTO film (Titre,NomRealisateur,PrenomRealisateur,duree,genre,note,synopsis) VALUES ('Test','Test',' test','0','test',3.4,'Lorsque les pires tyrans et les plus grands génies criminels de l’Histoire se réunissent pour planifier l’élimination de millions d’innocents, un homme se lance dans une course contre la montre pour contrecarrer leurs plans. ')";
         connect.executeUpdate(requeteAjout);
 
         String requete = "SELECT titre FROM film";
-        System.out.println(connect.remplirChampsRequete(requete));
+        System.out.println(connect.remplirChampsRequete(requete));*/
 
     }
 }
